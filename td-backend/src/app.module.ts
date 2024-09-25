ebb0866acd8cf0b373cfa32324a750082f8a59f8
christmknd <christmokonda@gmail.com>
2024-09-25 17:27:58 +0200

    Add all backend resources

--- td-backend/src/app.module.ts
+++ td-backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { ItemModule } from './item/item.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, ItemModule, CategoryModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
